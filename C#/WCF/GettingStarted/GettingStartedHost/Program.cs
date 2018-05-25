2b57d6ab245f6a43ad4f4d61057ead2f3b87a636
Song <503027010@logon.ds.ge.com>
2018-05-25 18:43:42 +0800

    update c# project

---
+++ C#/WCF/GettingStarted/GettingStartedHost/Program.cs
@@ -0,0 +1,41 @@
+﻿using GettingStartedLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStartedHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8000/ServiceModelSamples/Service");
+
+            ServiceHost serviceHost = new ServiceHost(typeof(CalculatorService), baseAddress);
+
+            try
+            {
+                serviceHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(SecurityMode.None), "CalculatorService");
+
+                serviceHost.Description.Behaviors.Add(new ServiceMetadataBehavior { HttpGetEnabled = true });
+
+                serviceHost.Open();
+
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                serviceHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine($"an exception is {ce.Message}");
+            }
+        }
+    }
+}
--- C#/webform_outputcache/aspOutOfCache/Default2.aspx
+++
@@ -1,8 +0,0 @@
-﻿<%@ Page Title="Home Page" Language="C#" AutoEventWireup="true" CodeBehind="Default2.aspx.cs" Inherits="aspOutOfCache._Default2" %>
-
-<%@ OutputCache Duration="10" VaryByParam="none" Location="Client" %>
-<br />
-<br />
-<br />
-<% string t = DateTime.Now.ToString(); Response.Write(t); %> 
-
